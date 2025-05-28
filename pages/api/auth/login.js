933ddd5db2acd23053717ae41cf3ee73f9b5fc33
Puzzle-Pals <puzzlepals@hotmail.com>
2025-05-28 01:40:27 -0500

    copilot fixing

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-// bcrypt hash for 'admin1029'
 const ADMIN_PASSWORD_HASH =
   process.env.ADMIN_PASSWORD_HASH ||
   "$2a$10$B9kE4e5hF6sK3p7Q8uHhOexuQp/2Gf9rQm6hC9kEoImzP6XkT6T7u";
